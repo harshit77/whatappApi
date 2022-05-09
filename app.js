785260abf886e22542bc0c575960bae249dcf7c9
Harshit Sahu <harshit.sahu@bigbinary.com>
2022-05-10 00:25:15 +0530

    Updated code

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-// const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode-terminal');
 const express = require('express');
 
-// const fs = require('fs');
-// const { Client,LocalAuth } = require('whatsapp-web.js');
+const { Client,LocalAuth } = require('whatsapp-web.js');
 const app = express();
 app.use(express.json());
 // app.use(function(req, res, next) {
@@ -19,54 +18,54 @@ app.use(express.json());
 //   next();
 // });
 // Use the saved values
-// const client = new Client({
-//     authStrategy: new LocalAuth(),
-//     puppeteer: { 	      headless: true,
-//                   args: ['--no-sandbox','--disable-setuid-sandbox']  },
-// }
-// );
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { 	      headless: true,
+                  args: ['--no-sandbox','--disable-setuid-sandbox']  },
+}
+);
 
 
-// client.on('qr', qr => {
-//     qrcode.generate(qr, {small: true});
-// });
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
 
-// client.on('ready', () => {
-//     console.log('Client is ready!');
-// //     sendMessageToNumber();
-//     client.on('message', message => {
-//         if(message.body === '!ping') {
-//             message.reply('pong');
-//         }
-//     });
-// });
+client.on('ready', () => {
+    console.log('Client is ready!');
+//     sendMessageToNumber();
+    client.on('message', message => {
+        if(message.body === '!ping') {
+            message.reply('pong');
+        }
+    });
+});
 
-// client.initialize();
+client.initialize();
 
-// const sendMessageToNumber= async (message,number) =>{
-//     const getNumberId= await client.getNumberId(number);
-//     if(getNumberId) {
-//         console.log(getNumberId)
-//      await client.sendMessage(getNumberId._serialized,message)
-//     }
-//     else {
-//         console.log("Mobile number is not registered")
-//     }
+const sendMessageToNumber= async (message,number) =>{
+    const getNumberId= await client.getNumberId(number);
+    if(getNumberId) {
+        console.log(getNumberId)
+     await client.sendMessage(getNumberId._serialized,message)
+    }
+    else {
+        console.log("Mobile number is not registered")
+    }
 
-// }
+}
 
 app.get("/",(req,res)=>res.send("Hello World"))
 
-// app.post('/sendmessage', async (req, res) => {
-//   try {
-//       console.log("Gothe request",req.body);
-//     const { number, message } = req.body; // Get the body
-//     const msg = await sendMessageToNumber(message,number); // Send the message
-//     res.send({ msg }); // Send the response
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+app.post('/sendmessage', async (req, res) => {
+  try {
+      console.log("Gothe request",req.body);
+    const { number, message } = req.body; // Get the body
+    const msg = await sendMessageToNumber(message,number); // Send the message
+    res.send({ msg }); // Send the response
+  } catch (error) {
+    next(error);
+  }
+});
 
 const PORT = process.env.PORT || 3050;
 app.listen(PORT,()=>console.log("Listening"))
