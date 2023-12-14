3cde17f96cd84a26bf8f3114aee903ab551a5527
Harshit Sahu <hsahu77@gmail.com>
2023-12-14 22:55:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,7 @@ const express = require('express');
 const { Client } = require('whatsapp-web.js');
 const app = express();
 app.use(express.json());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-//   res.header("Access-Control-Allow-Credentials", true); 
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(204);
-//   }
-//   next();
-// });
-// Use the saved values
+
 const client = new Client({
     puppeteer: { 	      headless: true,
                   args: ['--no-sandbox','--disable-setuid-sandbox']  },
